c1aa3dc3898d5d5e2a56b28a7adc43f1bb223fda
Hanxolotl <36183594+Hanxolotl@users.noreply.github.com>
2019-05-18 23:08:18 -0500

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 echo; echo "Downloading and installing kubectl";
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl 
 chmod +x ./kubectl;
 sudo mv ./kubectl /usr/local/bin/kubectl;
 kubectl version;
-if [ ! $? -eq 0]; then
+if [ ! $? -eq 0 ]; then
 	echo "There was an issue installing kubectl, please handle";
 	exit 1;
 fi
@@ -19,7 +21,7 @@ if [ -e ~/.minikube ]; then
 fi
 
 
-echo; echo "Downloading and installing minikube";
+echo; echo "Downloading and installing psql";
 sudo apt-get install -y postgresql-client;
 
-./start,sh
+./start.sh
